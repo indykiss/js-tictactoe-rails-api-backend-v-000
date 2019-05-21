4a06f22e5bc7786e40cbd74484276fff247f2ec1
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-21 17:57:52 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+
+  def create 
+  end 
+
+  def show 
+  end 
+
+  def updated 
+  end 
+
+  def index 
+  end 
+
 end
